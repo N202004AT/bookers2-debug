5bd7a9fa1373ac16b3fad73dfc9f576f45d94fe2
TAKERU ARAKAWA <takeruarakawa@TAKERUnoMacBook-Air.local>
2020-05-03 20:28:10 +0900

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,27 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, {only: [:edit,:update,]}
 
   def show
   	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
+    @user = current_user
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+      flash[:success]='successfully created book!'
+  		redirect_to book_path(@book.id) #保存された場合の移動先を指定。
   	else
   		@books = Book.all
-  		render 'index'
+  		render :index
   	end
   end
 
@@ -29,7 +36,7 @@ class BooksController < ApplicationController
   	if @book.update(book_params)
   		redirect_to @book, notice: "successfully updated book!"
   	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
-  		render "edit"
+  		render :edit
   	end
   end
 
@@ -39,10 +46,17 @@ class BooksController < ApplicationController
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
+  end
+
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title,:body,:user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root :to => 'homes#top'
+  get '/home/about' => 'homes#show'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  root :to => 'homes#top'
-  get 'home/about' => 'homes#about'
 end
